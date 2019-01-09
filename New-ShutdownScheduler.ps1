77923d6f15078b570cc2e3fedff2012997115e86
aaronamm <theeranitp@gmail.com>
2019-01-10 06:42:27 +0700

    Pass script command as flexible and fix log file date time not change

--- New-ShutdownScheduler.ps1
+++ New-ShutdownScheduler.ps1
@@ -1,7 +1,8 @@
 <#
 .SYNOPSIS
 Explanation: When you create the trigger with New-ScheduledTaskTrigger, 
-the time you specify is converted to UTC time and saved as a string in the trigger's StartBoundary property. On my machine, 10:15am produces a $trigger.StartBoundary of "2017-12-19T15:15:00Z", where the "Z" indicates UTC time. To specify a local time, we need to convert this date back into local time and remove the "Z"; 
+the time you specify is converted to UTC time and saved as a string in the trigger's StartBoundary property. On my machine, 10:15am produces a $trigger.StartBoundary of "2017-12-19T15:15:00Z", where the "Z" indicates UTC time. 
+To specify a local time, we need to convert this date back into local time and remove the "Z"; 
 we want "2017-12-19T10:15:00". 
 The function parses the date string, converts it to local time, and formats it in the correct format.
 Credit https://social.technet.microsoft.com/Forums/en-US/e1dad780-6a99-44f2-9688-041e7026854b/quotsynchronize-across-time-zonesquot-scheduled-task-option-and-newscheduledtasktrigger?forum=winserverpowershell
@@ -17,23 +18,23 @@ function Disable-SynchronizeTimeZone {
 }
 
 # Specify the trigger settings in UTC time, the instance need to set time to UTC
-$trigger = New-ScheduledTaskTrigger -At '6:50pm' -Daily | Disable-SynchronizeTimeZone #1.50am BKK time
-
+$trigger = New-ScheduledTaskTrigger -At "6:50pm" -Daily | Disable-SynchronizeTimeZone #1.50am BKK time
 $user = "NT AUTHORITY\SYSTEM" # Specify the account to run the script
-$utcNow = [System.DateTime]::UtcNow.ToString('yyyy-MM-ddTHH-mm-ssZ')
-$logName = "shutdown-$utcNow.txt"
 
-$command = 
-    "New-Item -Path 'c:/logs' -ItemType Directory -Force;" +
-    "'Shutdown at $utcNow' | Out-File 'c:/logs/$logName';" +
-    "Stop-Computer -Force"
+$command = {
+    New-Item -Path "c:/logs" -ItemType Directory -Force;
+    $utcNow = [System.DateTime]::UtcNow.ToString("yyyy-MM-ddTHH-mm-ssZ")
+    $logName = "shutdown-$utcNow.txt"
+    "Instance shutdown at $utcNow" | Out-File "c:/logs/$logName";
+    Stop-Computer -Force
+}
 
 $x = New-ScheduledTaskSettingsSet
 $x.UseUnifiedSchedulingEngine = $false
 
 $action = New-ScheduledTaskAction `
     -Execute "PowerShell.exe" `
-    -Argument "-Command $command" 
+    -Argument "-Command & { $command }" 
 
 Register-ScheduledTask `
     -TaskName "ShutdownTheInstance" `
