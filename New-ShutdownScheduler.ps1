d768756a426c282a7125bcf8774c60d9203bfe16
aaronamm <theeranitp@gmail.com>
2019-07-20 10:29:46 +0000

    Fix error of using double quotes in command statement

--- New-ShutdownScheduler.ps1
+++ New-ShutdownScheduler.ps1
@@ -1,15 +1,18 @@
 <#
 .SYNOPSIS
 Explanation: When you create the trigger with New-ScheduledTaskTrigger, 
-the time you specify is converted to UTC time and saved as a string in the trigger's StartBoundary property. On my machine, 10:15am produces a $trigger.StartBoundary of "2017-12-19T15:15:00Z", where the "Z" indicates UTC time. 
-To specify a local time, we need to convert this date back into local time and remove the "Z"; 
-we want "2017-12-19T10:15:00". 
+the time you specify is converted to UTC time and saved as a string in the trigger's StartBoundary property. 
+On my machine, 10:15am produces a $trigger. 
+StartBoundary of '2017-12-19T15:15:00Z', where the 'Z' indicates UTC time. 
+To specify a local time, we need to convert this date back into local time and remove the 'Z'; 
+we want '2017-12-19T10:15:00'. 
 
 The function parses the date string, converts it to local time, and formats it in the correct format.
 Credit https://social.technet.microsoft.com/Forums/en-US/e1dad780-6a99-44f2-9688-041e7026854b/quotsynchronize-across-time-zonesquot-scheduled-task-option-and-newscheduledtasktrigger?forum=winserverpowershell
 #>
 
-$timeToShutdownUtc = "6:50pm"
+#$timeToShutdownUtc = [System.DateTime]::UtcNow.AddMinutes(1).ToString('h:mmtt') # For testing
+$timeToShutdownUtc = '6:50pm'
 
 function Disable-SynchronizeTimeZone {
     param(
@@ -22,38 +25,36 @@ function Disable-SynchronizeTimeZone {
 }
 
 $command = {
+    $logPath = 'c:/logs'
+    New-Item -Path $logPath -ItemType Directory -Force # New folder if not exist
 
-    $logPath = "c:/logs"
     try {
-        $logFilePattern = "c:/logs/shutdown-*" 
-        $logFileLastMoreThanInDays = 30 #Remove log file last more than given day
+
+        $logFilePattern = Join-Path -Path $logpath -ChildPath 'shutdown-*'
+        $logFileLastMoreThanInDays = 7 #Remove log file last more than given day
         $utcNow = [System.DateTime]::UtcNow
-        $dateTimeFormat = "yyyy-MM-ddTHH-mm-ssZ";
+        $dateTimeFormat = 'yyyy-MM-ddTHH-mm-ssZ';
 
         Get-ChildItem -Path $logFilePattern | Where-Object {
-            $isMatch = $_ -match "(?<dateTime>\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}Z)"
+            $isMatch = $_ -match '(?<dateTime>\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}Z)'
             if ($isMatch) {
-                $logFileUtcDateTime = [System.DateTime]::parseexact($matches["dateTime"], $dateTimeFormat, $null)
+                $logFileUtcDateTime = [System.DateTime]::parseexact($matches['dateTime'], $dateTimeFormat, $null)
                 return $logFileUtcDateTime -lt $utcNow.AddDays( -1 * [System.Math]::Abs($logFileLastMoreThanInDays))
             }
             return $false
         } | Remove-Item -Force 
 
-        New-Item -Path $logPath -ItemType Directory -Force # New folder if not exist
 
-        $logName = "shutdown-$($utcNow.ToString($dateTimeFormat)).txt"
-        ("Instance shutdown at $($utcNow.ToString($dateTimeFormat))") | Out-File (Join-Path -Path $logPath -ChildPath $logName)
+        $logName = 'shutdown-{0}.txt' -f $utcNow.ToString($dateTimeFormat)
+        ('Instance shutdown at {0}' -f $utcNow.ToString($dateTimeFormat)) | `
+            Out-File (Join-Path -Path $logPath -ChildPath $logName)
 
         Stop-Computer -Force
     }
     catch {
-        $_.Exception | Out-File  "c:\logs\internal-error.txt"
+        $_.Exception | Out-File (Join-Path -Path $logPath -ChildPath 'internal-log.txt')
     }
 }
-
-#TODO comment for this function
-$setting = New-ScheduledTaskSettingsSet
-$setting.UseUnifiedSchedulingEngine = $false
 
 $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command & { $command }" 
 
